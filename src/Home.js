7bf66b3e5d32336335fb8eda8603e601fca3bb0c
Chance Smith <chancesmithb@gmail.com>
2019-04-30 09:30:49 -0500

    📦  NEW: add Grommet, Home, comments

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Heading } from "grommet";
+
+const Home = () => {
+  return (
+    <div>
+      <Heading>Best heading ever - should be gold</Heading>
+      <p>How can Grommet give styles/colors to all Headings in the app?</p>
+    </div>
+  );
+};
+
+export default Home;
