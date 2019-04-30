3e90b8d74842f8a8289a59733831b9c021a501bd
Chance Smith <chancesmithb@gmail.com>
2019-04-30 09:33:25 -0500

    📦  NEW: add HR in Home

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,12 @@ const Home = () => {
     <div>
       <Heading>Best heading ever - should be gold</Heading>
       <p>How can Grommet give styles/colors to all Headings in the app?</p>
+      <hr />
+      <p>
+        Similarly to Sylted-Components, how can I add SASS styles to globally
+        style all other HTML elements? Like: "<hr />
+        ".
+      </p>
     </div>
   );
 };
